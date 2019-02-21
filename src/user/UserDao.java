bb108bce8162a8a16a5899b922f71be4e8f844e5
MERT GEYIK <mert.geyik@consultant.turkcell.com.tr>
2019-02-21 22:38:49 +0300

    rest service was written

---
+++ src/user/UserDao.java
@@ -0,0 +1,55 @@
+package user;
+
+import java.io.File; 
+import java.io.FileInputStream; 
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream; 
+import java.io.IOException; 
+import java.io.ObjectInputStream; 
+import java.io.ObjectOutputStream; 
+import java.util.ArrayList; 
+import java.util.List;  
+
+public class UserDao { 
+   public List<User> getAllUsers(){ 
+      List<User> userList = null; 
+      try { 
+         File file = new File("Users.dat"); 
+         if (!file.exists()) { 
+            User mert = new User(1, "Mert", "Student"); 
+            User kubilay = new User(2, "Kubilay", "Programmer"); 
+            User dilek = new User(3, "Dilek", "Engineer");         
+            userList = new ArrayList<User>(); 
+            userList.add(mert); 
+            userList.add(kubilay); 
+            userList.add(dilek);            
+            saveUserList(userList); 
+         } 
+         else{ 
+            FileInputStream fis = new FileInputStream(file); 
+            ObjectInputStream ois = new ObjectInputStream(fis); 
+            userList = (List<User>) ois.readObject(); 
+            ois.close(); 
+         } 
+      } catch (IOException e) { 
+         e.printStackTrace(); 
+      } catch (ClassNotFoundException e) { 
+         e.printStackTrace(); 
+      }   
+      return userList; 
+   } 
+   private void saveUserList(List<User> userList){ 
+      try { 
+         File file = new File("Users.dat"); 
+         FileOutputStream fos;  
+         fos = new FileOutputStream(file); 
+         ObjectOutputStream oos = new ObjectOutputStream(fos); 
+         oos.writeObject(userList); 
+         oos.close(); 
+      } catch (FileNotFoundException e) { 
+         e.printStackTrace(); 
+      } catch (IOException e) { 
+         e.printStackTrace(); 
+      } 
+   }    
+}
